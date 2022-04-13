0aa55dc0fa1d1a4556f09ec68db187ab3916bc72
Erez Friemagor <erezfree29@gmail.com>
2022-04-13 23:05:27 +0100

    add refrence to photos,migrate refrence

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220413213756) do
+ActiveRecord::Schema.define(version: 20220413220413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,4 +21,13 @@ ActiveRecord::Schema.define(version: 20220413213756) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "photos", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "album_id"
+    t.index ["album_id"], name: "index_photos_on_album_id"
+  end
+
+  add_foreign_key "photos", "albums"
 end
